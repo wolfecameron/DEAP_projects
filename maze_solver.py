15a43fa9f3be4df02ee47e3249793ad391926293
Cameron Wolfe <wolfe.cameron@utexas.edu>
2017-10-10 20:09:55 -0500

    added result parsing algorithm to convert bit list into a sequence of printed movements

--- maze_solver.py
+++ maze_solver.py
@@ -14,16 +14,34 @@ maze = np.matrix([[0,0,0,0,0,0,0,0,0,0],
                   [0,0,0,0,1,1,1,1,1,0],
                   [0,0,0,0,0,0,0,0,0,0]])
 
+
+
+#translates resulting bitList into directions
+def parseResults(moveList):
+	for i in range(0,len(moveList),2):
+		if(moveList[i:i+2] == [0,0]):
+			print "North"
+		elif(moveList[i:i+2] == [1,0]):
+			print "East"
+		elif(moveList[i:i+2] == [0,1]):
+			print "South"
+		else:
+			print "West"
 				  
 #calculates final position based on list of moves by the individual
 
 '''
 Note:
-[0,0] -> South
+[0,0] -> North
 [1,0] -> East
-[0,1] -> North
+[0,1] -> South
 [1,1] -> West
 '''
+
+
+MAX_MOVES = 13 
+POP_SIZE = 200
+INDPB = .1 #probability of X-over for cxUniform tool/mutation
 
 def evalMatrix(individual):
     y = 1
@@ -54,24 +72,31 @@ def evalMatrix(individual):
             if maze[y,z] == 1:
                 x = z
     fitness = np.sqrt((((5-y)**2))+(((8-x)**2)))
-    return (1/(fitness+.01),)
+    return fitness,
+    #judges fitness based on final position in maze
 
-
-creator.create("FitnessMax", base.Fitness, weights = (+1.0,))
+	
+	
+creator.create("FitnessMax", base.Fitness, weights = (-1.0,))
 creator.create("Individual", list, fitness = creator.FitnessMax)
 tb = base.Toolbox()
 tb.register("bit", random.randint,0,1)
-tb.register("individual", tools.initRepeat, creator.Individual, tb.bit, n=30)
-tb.register("population", tools.initRepeat, list, tb.individual, n = 400)
+tb.register("individual", tools.initRepeat, creator.Individual, tb.bit, n=(MAX_MOVES*2))
+tb.register("population", tools.initRepeat, list, tb.individual, n= POP_SIZE)
 tb.register("evaluate", evalMatrix)
 tb.register("mate", tools.cxTwoPoint)
-tb.register("mutate", tools.mutFlipBit, indpb = 0.05)
+tb.register("mutate", tools.mutFlipBit, indpb = INDPB)
 tb.register("select", tools.selTournament, tournsize = 5)
 tb.register("map", map)
 
-cxpb , mutpb, ngen = .05, .05, 400
+
+
+cxpb , mutpb, ngen = .1, .1, 600
 pop = tb.population()
 pop = algorithms.eaSimple(pop,tb,cxpb,mutpb,ngen)
 
+#selects the first individual in the final population
 final_ind = pop[0][0]
-print final_ind
+
+#prints all moves made by this individual
+parseResults(final_ind)
